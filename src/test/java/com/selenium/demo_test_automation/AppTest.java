a75ee8659ec2f5cb420c902b708f6821385cc93c
devopsmvc <devops.mvc@gmail.com>
2022-11-22 13:58:05 +0530

    demo added

---
+++ src/test/java/com/selenium/demo_test_automation/AppTest.java
@@ -0,0 +1,20 @@
+package com.selenium.demo_test_automation;
+
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+}
